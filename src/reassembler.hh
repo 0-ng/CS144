3f73a57b5e821b4fcd4c634f92a2982eceace442
0ng <1427508598@qq.com>
2024-07-27 17:56:32 +0800

    lab1

--- src/reassembler.hh
+++ src/reassembler.hh
@@ -8,7 +8,8 @@ class Reassembler
 {
 public:
   // Construct Reassembler to write into given ByteStream.
-  explicit Reassembler( ByteStream&& output ) : output_( std::move( output ) ) {
+  explicit Reassembler( ByteStream&& output ) {
+    output_ = std::move( output )
     capacity_=output_.get_capacity();
     buffer=std::string(capacity_,' ');
     buffer_ready=std::vector<bool>(capacity_,false);
