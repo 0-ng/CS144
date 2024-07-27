2a00de2928dbe122a1899fdf60f1ff8566ffb370
0ng <1427508598@qq.com>
2024-07-27 16:40:10 +0800

    fix

--- src/byte_stream.cc
+++ src/byte_stream.cc
@@ -52,7 +52,8 @@ uint64_t Reader::bytes_popped() const
 string_view Reader::peek() const
 {
   if(head>=tail)return "";
-  return string_view(buffer[head%capacity_]);
+  return "";
+  // return string_view(buffer[head%capacity_]);
   // return buffer.substr(head%capacity_,1);
 }
 
