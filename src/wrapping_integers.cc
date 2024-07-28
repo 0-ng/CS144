148da1f756e76c8189229599fbdca02cf7fd9a71
0ng <1427508598@qq.com>
2024-07-28 10:21:20 +0800

    lab2

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -4,16 +4,23 @@ using namespace std;
 
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
-  // Your code here.
-  (void)n;
-  (void)zero_point;
-  return Wrap32 { 0 };
+  return zero_point+n;
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
-  // Your code here.
-  (void)zero_point;
-  (void)checkpoint;
-  return {};
+  uint64_t t=1;
+  t<<=32;
+  uint64_t base=(checkpoint>>32)<<32;
+  uint64_t ret=zero_point.raw_value_+base;
+  if(ret<checkpoint){
+    if(checkpoint-ret>ret+t-checkpoint){
+      ret+=t;
+    }
+  }else{
+    if(ret-checkpoint>checkpoint-(ret-t)){
+      ret-=t;
+    }
+  }
+  return ret;
 }
