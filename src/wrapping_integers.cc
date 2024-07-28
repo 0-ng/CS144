a752b0c2af2c8cc83d099d69732284da1ac1d13e
0ng <1427508598@qq.com>
2024-07-28 10:39:52 +0800

    lab2

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -9,7 +9,7 @@ Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
-  return raw_value_-zero_point.raw_value_+checkpoint;
+  return raw_value_-zero_point.raw_value_+(checkpoint>>32)<<32;
   // uint64_t t=1;
   // t<<=32;
   // uint64_t base=(checkpoint>>32)<<32;
