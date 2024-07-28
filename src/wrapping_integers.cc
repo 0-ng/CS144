bbebfcec8e79126691d65ed5d6a30916f74f85c6
0ng <1427508598@qq.com>
2024-07-28 11:04:10 +0800

    lab2

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -9,19 +9,15 @@ Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
-  return raw_value_-zero_point.raw_value_+((checkpoint>>32)<<32);
-  // uint64_t t=1;
-  // t<<=32;
-  // uint64_t base=(checkpoint>>32)<<32;
-  // uint64_t ret=zero_point.raw_value_+base;
-  // if(ret<checkpoint){
-  //   if(checkpoint-ret>ret+t-checkpoint){
-  //     ret+=t;
-  //   }
-  // }else{
-  //   if(ret-checkpoint>checkpoint-(ret-t)){
-  //     ret-=t;
-  //   }
-  // }
-  // return ret;
+  uint64_t ret=raw_value_-zero_point.raw_value_+((checkpoint>>32)<<32);
+  if(ret<checkpoint){
+    if(checkpoint-ret>ret+t-checkpoint){
+      ret+=t;
+    }
+  }else{
+    if(ret-checkpoint>checkpoint-(ret-t)){
+      ret-=t;
+    }
+  }
+  return ret;
 }
