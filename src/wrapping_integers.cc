a542db979a8d755573222a86492159aa9f851641
0ng <1427508598@qq.com>
2024-07-29 01:31:57 +0800

    lab2

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -13,10 +13,10 @@ uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
   uint64_t t=( 1UL << 32 );
   uint64_t b=ret+t;
   uint64_t s=ret-t;
-  if(max(b,checkpoint)-min(b,checkpoint)<max(ret,checkpoint)-min(ret,checkpoint)){
+  if(std::max(b,checkpoint)-std::min(b,checkpoint)<std::max(ret,checkpoint)-std::min(ret,checkpoint)){
     ret=b;
   }
-  if(max(s,checkpoint)-min(s,checkpoint)<max(ret,checkpoint)-min(ret,checkpoint)){
+  if(std::max(s,checkpoint)-std::min(s,checkpoint)<std::max(ret,checkpoint)-std::min(ret,checkpoint)){
     ret=s;
   }
   return ret;
